5ff75d9228a49d5dfb0b360ace3954703133b950
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-09-28 16:17:34 +0200

    Add files via upload

---
+++ 11_Rectangle Area.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int side1;
+	int side2;
+
+	cin >> side1;
+	cin >> side2;
+
+	int area = side1 * side2;
+
+	cout << area;
+
+	return 0;
+}

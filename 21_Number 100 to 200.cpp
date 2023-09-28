88d3e02b3c0110aded9b537fcf8a631b178418b3
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-09-28 16:30:09 +0200

    Add files via upload

---
+++ 21_Number 100 to 200.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num;
+
+	cin >> num;
+
+	if (num < 100)
+	{
+		cout << "Less than 100";
+	}
+	else if (num >= 100 && num <= 200)
+	{
+		cout << "Between 100 and 200";
+	}
+	else if(num > 200)
+	{
+		cout << "Greater than 200";
+	}
+}

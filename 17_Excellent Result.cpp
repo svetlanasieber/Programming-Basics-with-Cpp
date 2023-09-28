2f74f319af20f9451201024172ded45b891c62c1
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2023-09-28 16:25:46 +0200

    Add files via upload

---
+++ 17_Excellent Result.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double grade;
+
+    cin >> grade;
+
+    if (grade >= 5.50)
+    {
+        cout << "Excellent!";
+    }
+
+    return 0;
+}
